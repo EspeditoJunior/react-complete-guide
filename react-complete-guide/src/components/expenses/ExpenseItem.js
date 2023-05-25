f7145bd1284f05480405e66ae1ae7da4a60d0494
EspeditoJunior <e.jr42@hotmail.com>
2023-05-25 00:20:35 -0300

    primeiros componentes com estado

--- react-complete-guide/src/components/expenses/ExpenseItem.js
+++ react-complete-guide/src/components/expenses/ExpenseItem.js
@@ -1,18 +1,29 @@
 import './ExpenseItem.css'
+import Card from '../ui/Card';
+import { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
+
+    const [title, setTitle] = useState(props.title);
+
+    function clickHandler() {
+        setTitle(title + " oi");
+    }
+
+
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <div>
                 <ExpenseDate date={props.date}/>
             </div>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>R${props.amount}</div>
             </div>
-        </div>
+            <button onClick={clickHandler}>Change title</button>
+        </Card>
     )
 }
 
--- react-complete-guide/src/components/expenses/Expenses.js
+++ react-complete-guide/src/components/expenses/Expenses.js
@@ -1,12 +1,14 @@
 import ExpenseItem from './ExpenseItem'
+import Card from '../ui/Card';
+
 import './Expenses.css'
 
 function Expenses(props) {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
             <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
-        </div>
+        </Card>
     );
 }
 
