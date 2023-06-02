9e7e607fc82678a174632066c0611d69cf364401
EspeditoJunior <e.jr42@hotmail.com>
2023-06-01 23:31:31 -0300

    continuando o projeto

--- react-complete-guide/src/components/expenses/ExpenseItem.js
+++ react-complete-guide/src/components/expenses/ExpenseItem.js
@@ -1,30 +1,18 @@
 import './ExpenseItem.css'
 import Card from '../ui/Card';
-import { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
-
-    const [title, setTitle] = useState(props.title);
-
-    function clickHandler() {
-        setTitle(prevTitle => {
-            return prevTitle + " oi";
-        });
-    }
-
-
     return (
         <Card className='expense-item'>
             <div>
                 <ExpenseDate date={props.date}/>
             </div>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>R${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change title</button>
         </Card>
     )
 }
--- react-complete-guide/src/components/expenses/Expenses.js
+++ react-complete-guide/src/components/expenses/Expenses.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import ExpenseItem from './ExpenseItem'
 import Card from '../ui/Card';
 
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props) {
+
+    const [filter,setFilter] = useState('2020');
+
+    function saveFilter(selectedFilter){
+        console.log("filter aqui:" + selectedFilter);
+        setFilter(selectedFilter);
+    }
+
     return (
-        <Card className="expenses">
-            <ExpenseItem title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
-            <ExpenseItem title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter onSelectFilter={saveFilter} filter={filter} />
+                {
+                    props.expenses.map(e => <ExpenseItem title={e.title} amount={e.amount} date={e.date}/>)
+                }
+            </Card>
+        </div>
     );
 }
 
